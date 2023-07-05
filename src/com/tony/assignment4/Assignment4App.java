d933e1a5aa8b02597f16f48f893943d882735801
7onyG <124627151+7onyG@users.noreply.github.com>
2023-07-05 09:48:06 -0400

    final

--- src/com/tony/assignment4/Assignment4App.java
+++ src/com/tony/assignment4/Assignment4App.java
@@ -13,11 +13,27 @@ public class Assignment4App {
     String outputFile = "course1.txt";
 
     fileService.writeToFile(searchQuery, inputFile, outputFile);
+    
+    String searchQuery1 = "APMTH";
+    String inputFile1 = "student-master-list.csv";
+    String outputFile1 = "course2.txt";
+
+    fileService.writeToFile(searchQuery1, inputFile1, outputFile1);
+    
+    String searchQuery2 = "STAT";
+    String inputFile2 = "student-master-list.csv";
+    String outputFile2 = "course3.txt";
+
+    fileService.writeToFile(searchQuery2, inputFile2, outputFile2);
+    
+    //Please be patient
+    //Might need to run app a couple times
 
     	
-        // Read the student data from the input CSV file
         Student[] students = fileService.readStudentData(inputFile);
         System.out.println(Arrays.toString(students));
+        
+        //Making sure it works
         
 
     }
